b2564b4882be5fb24d1c927cc456ff00d896faa1
Claude Abounegm <claude.abounegm@gmail.com>
2016-05-16 16:25:11 +0200

    more stuff

--- Project3Digital/Scripts/Custom/main.js
+++ Project3Digital/Scripts/Custom/main.js
@@ -23,7 +23,7 @@ var defJSON;
 $(function () {
     "use strict";
 
-    defJSON = parseDEF('VERSION 5.6 ;\nNAMESCASESENSITIVE ON ;\nDIVIDERCHAR "/" ;\nBUSBITCHARS "<>" ;\nDESIGN counter_2bit ;\nUNITS DISTANCE MICRONS 100 ;\n\nDIEAREA ( -480.0 0.0 ) ( 7360.0 2400.0 ) ;\n\nTRACKS Y 0.0 DO 13 STEP 200 LAYER metal1 ;\nTRACKS X -480.0 DO 50 STEP 160.0 LAYER metal2 ;\nTRACKS Y 0.0 DO 13 STEP 200 LAYER metal3 ;\nTRACKS X -480.0 DO 25 STEP 320.0 LAYER metal4 ;\n\nCOMPONENTS 8 ;\n- BUFX2_2 BUFX2 + PLACED ( 80.0 100 ) S ;\n- DFFPOSX1_2 DFFPOSX1 + PLACED ( 560.0 100 ) S ;\n- AOI21X1_1 AOI21X1 + PLACED ( 2480.0 100 ) S ;\n- OAI21X1_1 OAI21X1 + PLACED ( 3120.0 100 ) FS ;\n- INVX1_1 INVX1 + PLACED ( 3760.0 100 ) S ;\n- NOR2X1_1 NOR2X1 + PLACED ( 4080.0 100 ) FS ;\n- DFFPOSX1_1 DFFPOSX1 + PLACED ( 4560.0 100 ) FS ;\n- BUFX2_1 BUFX2 + PLACED ( 6480.0 100 ) FS ;\nEND COMPONENTS\n\nPINS 4 ;\n- clk + NET clk\n  + LAYER metal2 ( 0 0 ) ( 1 1 )\n  + PLACED ( 2080.0 2400.0 ) N ;\n- reset + NET reset\n  + LAYER metal2 ( 0 0 ) ( 1 1 )\n  + PLACED ( 4160.0 2400.0 ) N ;\n- dout<0> + NET dout<0>\n  + LAYER metal3 ( 0 0 ) ( 1 1 )\n  + PLACED ( 7200.0 1200.0 ) N ;\n- dout<1> + NET dout<1>\n  + LAYER metal3 ( 0 0 ) ( 1 1 )\n  + PLACED ( -160.0 1200.0 ) N ;\nEND PINS\n\nNETS 10 ;\n- $auto$iopadmap.cc:164:execute$47<0>\n  ( DFFPOSX1_1 Q ) \n  ( NOR2X1_1 B ) \n  ( AOI21X1_1 A ) \n  ( OAI21X1_1 A ) \n  ( BUFX2_1 A ) \n+ ROUTED metal1 ( 6400 1170 ) ( * 1200 ) ( 6560 * ) \n  NEW metal1 ( 4480 1200 ) M2_M1 \n  NEW metal2 ( 4480 1200 ) M3_M2 \n  NEW metal3 ( 4480 1200 ) ( 6080 * ) M3_M2 \n  NEW metal2 ( 6080 1200 ) ( * 1400 ) M2_M1 \n  NEW metal1 ( 6080 1400 ) ( * 1510 ) \n  NEW metal1 ( 3040 1200 ) M2_M1 \n  NEW metal2 ( 3040 1200 ) M3_M2 \n  NEW metal3 ( 3040 1200 ) ( 4480 * ) \n  NEW metal1 ( 3200 1400 ) ( 3040 * ) M2_M1 \n  NEW metal2 ( 3040 1400 ) ( * 1200 ) ;\n- dout<0>\n  ( PIN dout<0> ) \n  ( BUFX2_1 Y ) \n+ ROUTED metal3 ( 7200 1200 ) ( 7040 * ) M3_M2 \n  NEW metal2 ( 7040 1200 ) ( * 1400 ) M2_M1 \n  NEW metal1 ( 7040 1400 ) ( 6880 * ) ;\n- $auto$iopadmap.cc:164:execute$47<1>\n  ( DFFPOSX1_2 Q ) \n  ( AOI21X1_1 B ) \n  ( OAI21X1_1 B ) \n  ( BUFX2_2 A ) \n+ ROUTED metal1 ( 640 1170 ) ( * 1200 ) ( 480 * ) \n  NEW metal1 ( 2880 1400 ) M2_M1 \n  NEW metal2 ( 2880 1400 ) M3_M2 \n  NEW metal3 ( 2880 1400 ) ( 960 * ) M3_M2 \n  NEW metal1 ( 960 1400 ) M2_M1 \n  NEW metal1 ( 960 1400 ) ( * 1510 ) \n  NEW metal1 ( 3360 1200 ) M2_M1 \n  NEW metal2 ( 3360 1200 ) ( * 1400 ) M3_M2 \n  NEW metal3 ( 3360 1400 ) ( 2880 * ) ;\n- dout<1>\n  ( PIN dout<1> ) \n  ( BUFX2_2 Y ) \n+ ROUTED metal3 ( -160 1200 ) ( 160 * ) M3_M2 \n  NEW metal2 ( 160 1200 ) ( * 1400 ) M2_M1 ;\n- reset\n  ( PIN reset ) \n  ( NOR2X1_1 A ) \n  ( INVX1_1 A ) \n+ ROUTED metal1 ( 4160 1600 ) ( 4000 * ) \n  NEW metal2 ( 4160 2400 ) ( * 1600 ) M2_M1 ;\n- _1_\n  ( OAI21X1_1 C ) \n  ( INVX1_1 Y ) \n+ ROUTED metal1 ( 3670 800 ) ( 3680 * ) ( 3840 * ) ;\n- _2_\n  ( AOI21X1_1 C ) \n  ( OAI21X1_1 Y ) \n+ ROUTED metal1 ( 2560 1600 ) M2_M1 \n  NEW metal2 ( 2560 1600 ) ( * 1200 ) M3_M2 \n  NEW metal3 ( 2560 1200 ) ( 2880 * ) M4_M3 \n  NEW metal4 ( 2880 1200 ) ( 3360 * ) ( * 1000 ) M4_M3 \n  NEW metal3 ( 3360 1000 ) ( 3520 * ) M3_M2 \n  NEW metal2 ( 3520 1000 ) ( 3360 * ) M2_M1 \n  NEW metal1 ( 3360 1000 ) ( 3450 * ) ;\n- _0_<1>\n  ( DFFPOSX1_2 D ) \n  ( AOI21X1_1 Y ) \n+ ROUTED metal1 ( 1920 1220 ) M2_M1 \n  NEW metal2 ( 1920 1200 ) ( * 1800 ) M3_M2 \n  NEW metal3 ( 1920 1800 ) ( 2720 * ) M3_M2 \n  NEW metal1 ( 2720 1800 ) M2_M1 ;\n- _0_<0>\n  ( DFFPOSX1_1 D ) \n  ( NOR2X1_1 Y ) \n+ ROUTED metal1 ( 4800 1220 ) M2_M1 \n  NEW metal2 ( 4800 1200 ) ( * 1400 ) M3_M2 \n  NEW metal3 ( 4800 1400 ) ( 4320 * ) M3_M2 \n  NEW metal1 ( 4320 1400 ) M2_M1 ;\n- clk\n  ( PIN clk ) \n  ( DFFPOSX1_2 CLK ) \n  ( DFFPOSX1_1 CLK ) \n+ ROUTED metal1 ( 2240 1400 ) M2_M1 \n  NEW metal2 ( 2240 1400 ) ( * 1600 ) M3_M2 \n  NEW metal3 ( 2240 1600 ) ( 4480 * ) M3_M2 \n  NEW metal2 ( 4480 1600 ) ( * 1400 ) M2_M1 \n  NEW metal1 ( 4480 1400 ) ( 4640 * ) ( 4650 * ) \n  NEW metal2 ( 2080 2400 ) ( 2240 * ) ( * 1600 ) ;\nEND NETS\n\nSPECIALNETS 5 ;\n- $auto$iopadmap.cc:164:execute$47<0>\n+ ROUTED metal1 80 ( 6400 1140 ) ( * 1230 ) \n  NEW metal1 80 ( 6080 1370 ) ( * 1540 ) ;\n- $auto$iopadmap.cc:164:execute$47<1>\n+ ROUTED metal1 80 ( 640 1140 ) ( * 1230 ) \n  NEW metal1 80 ( 960 1370 ) ( * 1540 ) ;\n- clk\n;\n- _1_\n;\n- _2_\n+ ROUTED metal4 120 ( 3360 1200 ) ( * 1000 ) \n  NEW metal1 80 ( 3330 1000 ) ( 3480 * ) ;\nEND SPECIALNETS\n\nEND DESIGN\n');
+    //defJSON = parseDEF('VERSION 5.6 ;\nNAMESCASESENSITIVE ON ;\nDIVIDERCHAR "/" ;\nBUSBITCHARS "<>" ;\nDESIGN counter_2bit ;\nUNITS DISTANCE MICRONS 100 ;\n\nDIEAREA ( -480.0 0.0 ) ( 7360.0 2400.0 ) ;\n\nTRACKS Y 0.0 DO 13 STEP 200 LAYER metal1 ;\nTRACKS X -480.0 DO 50 STEP 160.0 LAYER metal2 ;\nTRACKS Y 0.0 DO 13 STEP 200 LAYER metal3 ;\nTRACKS X -480.0 DO 25 STEP 320.0 LAYER metal4 ;\n\nCOMPONENTS 8 ;\n- BUFX2_2 BUFX2 + PLACED ( 80.0 100 ) S ;\n- DFFPOSX1_2 DFFPOSX1 + PLACED ( 560.0 100 ) S ;\n- AOI21X1_1 AOI21X1 + PLACED ( 2480.0 100 ) S ;\n- OAI21X1_1 OAI21X1 + PLACED ( 3120.0 100 ) FS ;\n- INVX1_1 INVX1 + PLACED ( 3760.0 100 ) S ;\n- NOR2X1_1 NOR2X1 + PLACED ( 4080.0 100 ) FS ;\n- DFFPOSX1_1 DFFPOSX1 + PLACED ( 4560.0 100 ) FS ;\n- BUFX2_1 BUFX2 + PLACED ( 6480.0 100 ) FS ;\nEND COMPONENTS\n\nPINS 4 ;\n- clk + NET clk\n  + LAYER metal2 ( 0 0 ) ( 1 1 )\n  + PLACED ( 2080.0 2400.0 ) N ;\n- reset + NET reset\n  + LAYER metal2 ( 0 0 ) ( 1 1 )\n  + PLACED ( 4160.0 2400.0 ) N ;\n- dout<0> + NET dout<0>\n  + LAYER metal3 ( 0 0 ) ( 1 1 )\n  + PLACED ( 7200.0 1200.0 ) N ;\n- dout<1> + NET dout<1>\n  + LAYER metal3 ( 0 0 ) ( 1 1 )\n  + PLACED ( -160.0 1200.0 ) N ;\nEND PINS\n\nNETS 10 ;\n- $auto$iopadmap.cc:164:execute$47<0>\n  ( DFFPOSX1_1 Q ) \n  ( NOR2X1_1 B ) \n  ( AOI21X1_1 A ) \n  ( OAI21X1_1 A ) \n  ( BUFX2_1 A ) \n+ ROUTED metal1 ( 6400 1170 ) ( * 1200 ) ( 6560 * ) \n  NEW metal1 ( 4480 1200 ) M2_M1 \n  NEW metal2 ( 4480 1200 ) M3_M2 \n  NEW metal3 ( 4480 1200 ) ( 6080 * ) M3_M2 \n  NEW metal2 ( 6080 1200 ) ( * 1400 ) M2_M1 \n  NEW metal1 ( 6080 1400 ) ( * 1510 ) \n  NEW metal1 ( 3040 1200 ) M2_M1 \n  NEW metal2 ( 3040 1200 ) M3_M2 \n  NEW metal3 ( 3040 1200 ) ( 4480 * ) \n  NEW metal1 ( 3200 1400 ) ( 3040 * ) M2_M1 \n  NEW metal2 ( 3040 1400 ) ( * 1200 ) ;\n- dout<0>\n  ( PIN dout<0> ) \n  ( BUFX2_1 Y ) \n+ ROUTED metal3 ( 7200 1200 ) ( 7040 * ) M3_M2 \n  NEW metal2 ( 7040 1200 ) ( * 1400 ) M2_M1 \n  NEW metal1 ( 7040 1400 ) ( 6880 * ) ;\n- $auto$iopadmap.cc:164:execute$47<1>\n  ( DFFPOSX1_2 Q ) \n  ( AOI21X1_1 B ) \n  ( OAI21X1_1 B ) \n  ( BUFX2_2 A ) \n+ ROUTED metal1 ( 640 1170 ) ( * 1200 ) ( 480 * ) \n  NEW metal1 ( 2880 1400 ) M2_M1 \n  NEW metal2 ( 2880 1400 ) M3_M2 \n  NEW metal3 ( 2880 1400 ) ( 960 * ) M3_M2 \n  NEW metal1 ( 960 1400 ) M2_M1 \n  NEW metal1 ( 960 1400 ) ( * 1510 ) \n  NEW metal1 ( 3360 1200 ) M2_M1 \n  NEW metal2 ( 3360 1200 ) ( * 1400 ) M3_M2 \n  NEW metal3 ( 3360 1400 ) ( 2880 * ) ;\n- dout<1>\n  ( PIN dout<1> ) \n  ( BUFX2_2 Y ) \n+ ROUTED metal3 ( -160 1200 ) ( 160 * ) M3_M2 \n  NEW metal2 ( 160 1200 ) ( * 1400 ) M2_M1 ;\n- reset\n  ( PIN reset ) \n  ( NOR2X1_1 A ) \n  ( INVX1_1 A ) \n+ ROUTED metal1 ( 4160 1600 ) ( 4000 * ) \n  NEW metal2 ( 4160 2400 ) ( * 1600 ) M2_M1 ;\n- _1_\n  ( OAI21X1_1 C ) \n  ( INVX1_1 Y ) \n+ ROUTED metal1 ( 3670 800 ) ( 3680 * ) ( 3840 * ) ;\n- _2_\n  ( AOI21X1_1 C ) \n  ( OAI21X1_1 Y ) \n+ ROUTED metal1 ( 2560 1600 ) M2_M1 \n  NEW metal2 ( 2560 1600 ) ( * 1200 ) M3_M2 \n  NEW metal3 ( 2560 1200 ) ( 2880 * ) M4_M3 \n  NEW metal4 ( 2880 1200 ) ( 3360 * ) ( * 1000 ) M4_M3 \n  NEW metal3 ( 3360 1000 ) ( 3520 * ) M3_M2 \n  NEW metal2 ( 3520 1000 ) ( 3360 * ) M2_M1 \n  NEW metal1 ( 3360 1000 ) ( 3450 * ) ;\n- _0_<1>\n  ( DFFPOSX1_2 D ) \n  ( AOI21X1_1 Y ) \n+ ROUTED metal1 ( 1920 1220 ) M2_M1 \n  NEW metal2 ( 1920 1200 ) ( * 1800 ) M3_M2 \n  NEW metal3 ( 1920 1800 ) ( 2720 * ) M3_M2 \n  NEW metal1 ( 2720 1800 ) M2_M1 ;\n- _0_<0>\n  ( DFFPOSX1_1 D ) \n  ( NOR2X1_1 Y ) \n+ ROUTED metal1 ( 4800 1220 ) M2_M1 \n  NEW metal2 ( 4800 1200 ) ( * 1400 ) M3_M2 \n  NEW metal3 ( 4800 1400 ) ( 4320 * ) M3_M2 \n  NEW metal1 ( 4320 1400 ) M2_M1 ;\n- clk\n  ( PIN clk ) \n  ( DFFPOSX1_2 CLK ) \n  ( DFFPOSX1_1 CLK ) \n+ ROUTED metal1 ( 2240 1400 ) M2_M1 \n  NEW metal2 ( 2240 1400 ) ( * 1600 ) M3_M2 \n  NEW metal3 ( 2240 1600 ) ( 4480 * ) M3_M2 \n  NEW metal2 ( 4480 1600 ) ( * 1400 ) M2_M1 \n  NEW metal1 ( 4480 1400 ) ( 4640 * ) ( 4650 * ) \n  NEW metal2 ( 2080 2400 ) ( 2240 * ) ( * 1600 ) ;\nEND NETS\n\nSPECIALNETS 5 ;\n- $auto$iopadmap.cc:164:execute$47<0>\n+ ROUTED metal1 80 ( 6400 1140 ) ( * 1230 ) \n  NEW metal1 80 ( 6080 1370 ) ( * 1540 ) ;\n- $auto$iopadmap.cc:164:execute$47<1>\n+ ROUTED metal1 80 ( 640 1140 ) ( * 1230 ) \n  NEW metal1 80 ( 960 1370 ) ( * 1540 ) ;\n- clk\n;\n- _1_\n;\n- _2_\n+ ROUTED metal4 120 ( 3360 1200 ) ( * 1000 ) \n  NEW metal1 80 ( 3330 1000 ) ( 3480 * ) ;\nEND SPECIALNETS\n\nEND DESIGN\n');
 
     // the Raphael canvas variable
     var paper;
@@ -53,7 +53,7 @@ $(function () {
                 .colorpicker()
                 // event to change CSS on color change.
                 .on("changeColor", function (e) {
-                    $.rule('.highlight.' + $this.data("name"), '#customStyle').css("fill", e.color);
+                    $.rule('.highlight.' + $this.data("name"), '#customStyle').css($this.data("ctype"), e.color);
                 })
                 // we set the default value of the colorpicker after creating it
                 // to trigger the "changeColor" event. This sets the correct CSS
@@ -69,26 +69,6 @@ $(function () {
                 .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
         });
     }();
-
-    // Gets the default highlight color for a specific type.
-    //function getDefaultColorFor(type) {
-    //    if (type === "FILL")
-    //        return "#888888";
-
-    //    if (type.indexOf("DFF") > -1)
-    //        return "yellow";
-
-    //    if (type.indexOf("metal1") > -1)
-    //        return "#10FF10";
-
-    //    if (type.indexOf("metal2") > -1)
-    //        return "orange";
-
-    //    if (type.indexOf("metal3") > -1)
-    //        return "red";
-
-    //    return "hotpink";
-    //}
 
     // from: http://blog.adamcole.ca/2011/11/simple-javascript-rainbow-color.html
     // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). 
@@ -212,13 +192,39 @@ $(function () {
             $node.click(onClick);
         }
 
+        var netsTypes = {};
+        -function () {
+            var nets = defJSON.nets;
+
+            nets.forEach(function (net) {
+                net.routes.forEach(function (route) {
+                    var layer = route.layer;
+                    netsTypes['l' + layer] = void 0;
+
+                    var pathString = [];
+                    route.coords.forEach(function (coord, i) {
+                        pathString.push(i === 0 ? 'M' : 'L');
+                        pathString.push(xOff + coord.x * wS);
+                        pathString.push(',');
+                        pathString.push(yOff - coord.y * hS);
+                    });
+
+                    $(paper.path(pathString.join('')).node)
+                        .addClass("l" + layer);
+                });
+            });
+        }();
+
         // for each array which contains the names of the pins and cells,
         // we need to add them to the accordion. So we do that now.
         -function () {
-            function AddCheckboxesToAccordion(arr, containerName) {
+            function AddCheckboxesToAccordion(arr, containerName, colorType, fn) {
                 var keys = Object
                     .keys(arr)
                     .sort();
+
+                var names = keys;
+                if(fn) names = names.map(fn);
 
                 keys.forEach(function (key, index) {
                     // we instantiate a new entry for each item, so when
@@ -254,7 +260,7 @@ $(function () {
                             }))
                             
                             // <span> {key}</span>
-                            .append("<span> " + key + "</span>")
+                            .append("<span> " + names[index] + "</span>")
                         )
 
                         //    <span class="picker" data-color="{color}" data-name="{key}">
@@ -264,6 +270,7 @@ $(function () {
                             $('<span class="picker" />')
                                 .attr("data-color", color)
                                 .attr("data-name", key)
+                                .attr("data-ctype", colorType)
                                 .append('<span class="add-on"><i></i></span>')
                         )
                     );
@@ -271,8 +278,9 @@ $(function () {
             }
 
             // We add the checkboxes in different accordions
-            AddCheckboxesToAccordion(cellTypes, "cellsContainer");
-            AddCheckboxesToAccordion(pinTypes, "pinsContainer");
+            AddCheckboxesToAccordion(cellTypes, "cellsContainer", 'fill');
+            AddCheckboxesToAccordion(pinTypes, "pinsContainer", 'fill');
+            AddCheckboxesToAccordion(netsTypes, "netsContainer", 'stroke', function(a) { return a.substring(1,a.length); });
         }();
 
         // we register an event handler with the show and hide buttons, to show/hide
